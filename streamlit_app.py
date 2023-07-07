aa3cd5724c8855fdc9e7d9a45acb961df049e8e0
Randy L <49382361+digitalstun@users.noreply.github.com>
2023-07-07 12:35:31 -0400

    Update streamlit_app.py
    code clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,9 @@ icon_size = 20
 
 st_button('youtube', 'https://www.youtube.com/channel/UCnAUn4WRpHu6Y7XR0O26iEQ', 'Cinematography and Film', icon_size)
 st_button('bigcartel', 'https://mrlafayette.bigcartel.com/products', 'Visit Big Cartel Store', 16)
-
 st_button('instagram', 'https://www.instagram.com/mr_lafayette', 'Follow me on Instagram', '24')
-
-# st_button('Shop', 'https://mrlafayette.bigcartel.com/products', 'Shop T-Shirts, Prints & Apparel', icon_size)
-# st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-# st_button('medium', 'https://', 'Read my blogs, icon_size)
 st_button('twitter', 'https://twitter.com/digitalstun', 'Follow me on Twitter', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/randolph-lafayette-303721139/', 'Follow me on LinkedIn', icon_size)
+# st_button('medium', 'https://', 'Read my blogs, icon_size)
 # st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
 # st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
