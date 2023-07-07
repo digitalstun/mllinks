65f83ff2aeb369b526b307cc6772251a71e676bb
Randy L <49382361+digitalstun@users.noreply.github.com>
2023-07-06 20:33:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.info('Developer, Director, Cinematographer, Support Specialist')
 icon_size = 20
 
 st_button('youtube', 'https://www.youtube.com/channel/UCnAUn4WRpHu6Y7XR0O26iEQ', 'Cinematograpy and Film YouTube channel', icon_size)
-# st_button('Store', 'https://mrlafayette.bigcartel.com/products', 'Shop Tees & Apparel', icon_size)
+st_button('Store', 'https://mrlafayette.bigcartel.com/products', 'Shop Tees & Apparel', icon_size)
 # st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
 # st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
 # st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
